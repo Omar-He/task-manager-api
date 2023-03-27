ba8380c636dbcac635c2a8afefa7a6fe5c0359f7
Omar-He <omar.hendi92@gmail.com>
2023-03-27 17:41:11 +0300

    reset/forgot password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,85 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const token = user.createJWT();
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 3600000; // token expires in 1 hour
+    await user.save();
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: email,
+      subject: "Reset your password",
+      // text: `Click on this link to reset your password: ${process.env.CLIENT_URL}/reset-password/${token}`,
+      html: `
+      <p>Hello,</p>
+      <p>We received a request to reset your password for your account at the Task Manager. To reset your password, please click on the following link:</p>
+      <p><a href="https://task-manager-app-mu.vercel.app/reset-password?token=${token}">https://task-manager-app-mu.vercel.app/reset-password?token=${token}</a></p>
+      <p>Please note that this link will expire in 1 hour.</p>
+      <p>If you did not make this request, you can safely ignore this email and your password will remain unchanged.</p>
+      <p>Best regards,</p>
+      <p>The lab Team</p>
+    `,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Error sending email" });
+      }
+
+      res.json({ message: "Email sent successfully" });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  const { password, token } = req.body;
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({
+      _id: payload.userId,
+    });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ error: "Password reset token is invalid or has expired" });
+    }
+
+    user.password = password;
+    user.resetPasswordToken = null;
+    user.resetPasswordExpires = null;
+    await user.save();
+
+    res.json({ message: "Password reset successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -28,6 +107,8 @@ const login = async (req, res) => {
 };
 
 module.exports = {
+  forgotPassword,
+  resetPassword,
   register,
   login,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/auth");
+const {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/auth");
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
